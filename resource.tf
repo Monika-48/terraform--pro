d6544cf2800f82282fb147fe2c8dc6f38c3373c0
pillamonikadurga@gmail.com <pillamonikadurga@gmail.com>
2025-07-17 11:27:31 +0000

    first-commit

--- resource.tf
+++ resource.tf
@@ -3,10 +3,10 @@ resource "azurerm_resource_group" "example" {
   location = "East US"
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestoracc"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+resource "azurerm_storage_account" "storage" {
+  name                     = "mystorageacct2409"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }

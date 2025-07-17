a1be348204a544e54fd998ebfa78ca1a746d7fb9
pillamonikadurga@gmail.com <pillamonikadurga@gmail.com>
2025-07-17 11:46:15 +0000

    first-commit

--- resource.tf
+++ resource.tf
@@ -10,3 +10,8 @@ resource_group_name = azurerm_resource_group.example.name
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+resource "azurerm_storage_container" "example" {
+  name                  = "example-container2409"
+storage_account_name = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
